cf0d94ff70a473baa548ded14c83679b4dc38607
Camelia STINGA <camelia.stinga@Digital-Grenoble.local>
2021-06-07 15:39:21 +0200

    query-9

--- results/queries/query-9.sql
+++ results/queries/query-9.sql
@@ -1,4 +1,4 @@
-SELECT orders.number, sum(order_product.quantity*products.price) AS totalPrice, customers.first_name
+SELECT orders.number, sum(order_product.quantity*products.price) AS totalPrice
 FROM orders 
 INNER JOIN order_product ON orders.id=order_product.order_id
 INNER JOIN products ON order_product.product_id=products.id
